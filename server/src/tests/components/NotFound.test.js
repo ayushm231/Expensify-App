69371ba39456763f7d94dcb13fd11e2b3abbc680
Ayush <ayushm231@gmail.com>
2021-08-30 16:58:53 +0530

    Deleted some files to fix bugs

--- server/src/tests/components/NotFound.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import NotFoundPage from '../../components/NotFound';
-
-test('Should render NotFound correctly', () => {
-    const wrapper = shallow(<NotFoundPage />);
-    expect(wrapper).toMatchSnapshot();
-});
